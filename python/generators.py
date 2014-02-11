f3979d1cc875970cf64839c8d7a8a364021fcac0
Rui Vaz <rui.wax@gmail.com>
2014-02-11 10:19:06 -0800

    Adds Fibonaci Generator

--- python/generators.py
+++ python/generators.py
@@ -1,7 +1,25 @@
+import sys
+
 def cubic_generator(n):
   for i in range(n):
     yield i ** 3
 
 
+def fibonaci(n):
+  if(n<3): return n
+  else: return fibonaci(n-1)+fibonaci(n-2) 
+
+
+def n_fibonaci(n):
+  for i in range(n):
+    yield fibonaci(i)  
+
 for i in cubic_generator(10):
   print i
+
+i=0
+for i in n_fibonaci(int(sys.argv[1])):
+  i+=i
+
+print i
+
